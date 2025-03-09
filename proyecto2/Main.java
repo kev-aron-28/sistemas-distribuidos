a9919988cbb4f46fef4d4c80298f7bfe007ffd65
kev-aron-28 <kevtapia75@gmail.com>
2025-03-08 21:58:12 -0600

    proyecto 2

--- proyecto2/Main.java
+++ proyecto2/Main.java
@@ -44,7 +44,6 @@ public class Main extends JPanel {
             plane.updateMovement();
         }
 
-        // Verificar colisiones
         for (Airplane chaser : planes) {
             if (chaser != mainPlain && chaser.hasCollided(mainPlain)) {
                 
